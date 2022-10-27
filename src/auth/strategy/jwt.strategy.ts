841db03af2141b13e6903a9b24b2cf66d48ac015
ngimdock <ngimdockzemfack@gmail.com>
2022-10-27 21:52:03 +0100

    Gestion de la route privé de l'utilisateur user/me

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -2,14 +2,26 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private config: ConfigService) {
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(config: ConfigService, private prismaService: PrismaService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: config.get('JWT_SECRET'),
     });
+  }
+
+  async validate(payload: { sub: number; email: string }) {
+    const currentUser = await this.prismaService.user.findUnique({
+      where: {
+        id: payload.sub,
+      },
+    });
+
+    delete currentUser.hash;
+    return currentUser;
   }
 }
